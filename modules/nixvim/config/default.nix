89254fb419d91f3cdeec904e489037baaf878155
Jamie Kemman <jamieruth@proton.me>
2024-04-29 01:30:12 -0400

    adding nixvim config

--- modules/nixvim/config/default.nix
+++ modules/nixvim/config/default.nix
@@ -3,4 +3,26 @@
   imports = [
     ./bufferline.nix
   ];
+
+  colorschemes.gruvbox.enable = true;
+
+  plugins = {
+    lualine.enable = true;
+
+    telescope.enable = true;
+
+    oil.enable = true;
+
+    treesitter.enable = true;
+  };
+
+  plugins.lsp = {
+    enable = true;
+    servers = {
+
+      rust-analyzer.enable = true;
+
+      pylyzer.enable = true;
+
+      java-language-server.enable = true;
 }
