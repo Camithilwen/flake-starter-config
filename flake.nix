fc42649e707dc48e529714a7e1a320c9ff532b74
Jamie Kemman <jamieruth@proton.me>
2024-05-09 11:18:57 -0400

    still installing home manager

--- flake.nix
+++ flake.nix
@@ -5,19 +5,19 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
    # home-manager.url = "github:nix-community/home-manager";
    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.url = "github:nix-community/nixvim"; # Correct placement of NixVim flake input
-    nixvim.inputs.nixpkgs.follows = "nixpkgs"; # Ensure NixVim follows the same Nixpkgs version
+   # nixvim.url = "github:nix-community/nixvim"; # Correct placement of NixVim flake input
+   # nixvim.inputs.nixpkgs.follows = "nixpkgs"; # Ensure NixVim follows the same Nixpkgs version
   };
 
-  outputs = { self, nixpkgs, nixvim, ... } @ inputs: {
+  outputs = { self, ... } @ inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux"; # Specify your system type here
       specialArgs = { inherit inputs; }; # Pass inputs to the configuration
       modules = [
         ./hosts/default/configuration.nix
 #        home-manager.nixosModules.home-manager
-       inputs. nixvim.nixosModules.nixvim # Using the NixVim module from the flake
-        /etc/nixos/modules/nixvim/config/default.nix # Directly include your existing NixVim config
+#       inputs. nixvim.nixosModules.nixvim # Using the NixVim module from the flake
+#        /etc/nixos/modules/nixvim/config/default.nix # Directly include your existing NixVim config
       ];
     };
   };
